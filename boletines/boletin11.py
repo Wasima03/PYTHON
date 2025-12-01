fa32570c38231a531b9c8f7055b9c2a5a6f73659
Wasima03 <elouastaniaznag@gmail.com>
2025-12-02 00:24:37 +0100

    02/12

--- boletines/boletin11.py
+++ boletines/boletin11.py
@@ -1,13 +1,60 @@
 class Pokemon:
 
     lista={"Normal","Agua","Fuego","Planta","Volador","Lucha","Veneno","Eléctrico","Tierra","Roca","Psíquico","Hielo","Bicho","Fantasma","Dragón"}
-    def __init__(self,codigo,nombre,tipos):
-        if codigo>=1 or codigo <=151:
+    def __init__(self,codigo,nombre,tipos,evolucion):
+        if 1<= codigo <=151:
             self.codigo=codigo
         else:
             self.codigo=1
+
         self.nombre=nombre
-        if len(lista | tipos)>=1 or len(lista | tipos)<=2:
+
+        if 1<= len(self.lista & set(tipos)) <=2:
             self.tipos=tipos
+        else:
+            self.tipos="Normal"
+        self.evolucion=evolucion
+
+    def getCodigo(self):
+        return (self.codigo)
+    def getNombre(self):
+        return (self.nombre)
+    def getTipos(self):
+        return(self.tipos)
+    def evoluciona(self):
+        return (self.evolucion)
+
+pokemon = Pokemon(1, "Bulbasaur", ["Planta", "Veneno", "Fuego", "Magia"],"jo")
+print(pokemon.getTipos())
+pokemon2 = Pokemon(2, "Bulbasaur", ["Fuego", "Agua"],pokemon.getCodigo())
+print(pokemon2.getTipos())
+print(pokemon2.getNombre())
+print(pokemon2.getCodigo())
+print(pokemon2.evoluciona())
+
+class Manga:
+    genero={"shonen", "shojo", "seinen", "josei", "kodomo", "yuri", "spokon", "isekai" , "hentai"}
+    def __init__(self,autor,genero,numero,*titulos):
+        self.autor=autor
+        self.numero=numero
+        if(1<=len(titulos)<=2):
+            self.titulos=titulos
+        if(len(self.genero & set(genero))==1):
+            self.genero=genero
+
+    def getAutor(self):
+        return (self.autor)
+    def getGenero(self):
+        return (self.genero)
+    def getNumero(self):
+        return (self.numero)
+    def getTitulos(self):
+        return (self.titulos)
+    def setNumero(self,num):
+        self.numero=num
+    def __add__(self, titulo):
+        self.titulos=self.titulos+titulo
+
+
 
 
