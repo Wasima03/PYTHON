434c4977267e8e6caf48cf8e9e72e54cf4e715d1
Wasima03 <elouastaniaznag@gmail.com>
2025-12-05 00:11:29 +0100

    05/12

--- boletines/boletin11_Manga.py
+++ boletines/boletin11_Manga.py
@@ -1,10 +1,11 @@
 class Manga:
     listaGeneros={"shonen", "shojo", "seinen", "josei","kodomo", "yuri", "spokon", "isekai", "henta"}
+    numeros=list()
     def __init__(self,autor,titJap,genero,numero,titEsp=None):
         self.__autor=autor
         self.__titJap=titJap
         self.__titEsp=titEsp
-        self.__numero=numero
+        self.__ultimoNumero=numero
 
         if genero in self.listaGeneros:
             self.__genero=genero
@@ -15,23 +16,53 @@ class Manga:
 
     #GETTERS
     def getAutor(self):
-        return (self.__autor)
+        return self.__autor
     def getTitJap(self):
-        return (self.__titJap)
+        return self.__titJap
     def getTitEsp(self):
-        return (self.__titEsp)
+        return self.__titEsp
     def getNumero(self):
-        return (self.__numero)
+        return self.numeros
     def getGenero(self):
-        return (self.__genero)
+        return self.__genero
 
     #SETTERS
     def setTitEsp(self,titulo):
         if(self.__titEsp==None):
             self.__titEsp=titulo
-    def setNumeros(self,*numeros):
-        for numero in numeros:
-            self.__numeros=self.__numeros+numero
+
+    def setNumeros(self,*num):
+        for n in num:
+            self.__numeros=self.numeros.append(n)
+            self.numeros.sort()
+
+    def completarColeccion(self):
+        nums=list()
+        for i in range (1,self.__ultimoNumero+1):
+            if i not in self.numeros:
+                nums.append(i)
+        return nums
+
+    def eliminarNumero(self,num):
+        esta=False
+        for i in self.numeros:
+            if i==num:
+                self.numeros.remove(num)
+                esta=True
+        if(esta):
+            print("Numero eliminado de la coleccion")
+        else:
+            print("No se ha encontrado el número a eliminar")
+
 
 m=Manga("e","d","cw",10)
 m.setNumeros(1,2,3)
+print(m.completarColeccion())
+print(m.getNumero())
+print(m.getTitJap())
+print(m.getTitEsp())
+m.setTitEsp("f")
+print(m.getTitEsp())
+m.eliminarNumero(6)
+m.eliminarNumero(1)
+
--- boletines/boletin8.py
+++ boletines/boletin8.py
@@ -1,9 +1,13 @@
-matriz=[[1,2],[3,4]]
+matriz=[[1,2],[3,4],[5,6]]
 traspuesta=[]
 
-for i in matriz:
-    for j in i:
-        traspuesta=[[j][i]]
-        print("|",j,end=" ")
-    print("|")
+print(len(matriz))
+print(len(matriz[1]))
+print(matriz[0])
+print(matriz[1])
+print(traspuesta)
+
+for i in range (len(matriz)):
+    for j in range (len(matriz[0])):
+        traspuesta.append(matriz[j][i])
 print(traspuesta)
