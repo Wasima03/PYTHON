c20a2bc1c88c51e2e77d27c9bebbbd14f1b7f963
Wasima03 <elouastaniaznag@gmail.com>
2025-12-08 23:38:58 +0100

    09/12

--- boletines/boletin8.py
+++ boletines/boletin8.py
@@ -1,3 +1,5 @@
+#1
+"""
 matriz=[[1,2],[3,4],[5,6]]
 traspuesta=[]
 
@@ -11,3 +13,40 @@ for i in range (len(matriz)):
     for j in range (len(matriz[0])):
         traspuesta.append(matriz[j][i])
 print(traspuesta)
+
+#2
+valida=False
+min=False
+may=False
+num=False
+simbolo=False
+simbolos = {"_","-", "!", "?", "*"}
+
+while(not valida):
+    pasword = input("Introduce una contrseña")
+    for i in pasword:
+        if i.islower():
+            min=True
+        elif i.isupper():
+            may=True
+        elif i.isdigit():
+            num=True
+    if len(set(pasword) & simbolos)>=1:
+        simbolo=True
+    if (8 <= len(pasword) <= 20):
+        if (min and may and num and simbolo):
+            valida=True
+            print("Contrseña valida")
+"""
+
+#3
+mensaje = input("Escribe tu mensaje secreto")
+cifrado=""
+for i in mensaje:
+    cifrado += i
+    for j in mensaje(-1,-1,-1):
+        cifrado +=j
+print(cifrado)
+
+
+
